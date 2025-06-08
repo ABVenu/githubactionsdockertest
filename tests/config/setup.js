d3d985c8668166badefeef3c538d8438e781fe28
abvenu <avenugopalb@gmail.com>
2025-06-08 17:58:07 +0530

    After all fb logic replaced

--- tests/config/setup.js
+++ tests/config/setup.js
@@ -1,21 +1,29 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-/// setup.js will be sensed by jest because its path is specified in 
+/// setup.js will be sensed by jest because its path is specified in
 // package.json under key called as jest
 
 // before All will work before starying the test
 /// before starting the test, MONGO should be connected
 beforeAll(async () => {
-  await mongoose.connect(process.env.MONGO_URI)
-  console.log(process.env.MONGO_URI)
-  console.log("Connected To DB In Jest Config")
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log(process.env.MONGO_URI);
+  console.log("Connected To DB In Jest Config");
 });
 
-
-// Once testing is finished, clean the DB & close the connection 
+// Once testing is finished, clean the DB & close the connection
 
 afterAll(async () => {
-  await mongoose.connection.dropDatabase(); 
+  // await mongoose.connection.dropDatabase();
+  // await mongoose.connection.close();
+  // console.log("DB Deleted and connection closed")
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+
   await mongoose.connection.close();
-  console.log("DB Deleted and connection closed")
+
+  console.log("Collections cleared and connection closed");
 });
